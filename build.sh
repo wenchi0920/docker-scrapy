71150a4ce4723205b294df158d94a853cb3136a7
wenchi <wenchi@sitetag.us>
2020-10-13 11:11:30 +0800

    build tag shell

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dname=$(/usr/bin/dirname $0)
+dname=$(/bin/readlink -f "$dname")
+
+
+IFS=$'\n'
+for tag in `cat "${dname}/tag" | sort` ;
+do
+    echo "${tag}"
+    cat "${dname}/Dockerfile.sample" | sed "s/{{VER}}/$tag/" > "${dname}/Dockerfile"
+    git commit "${dname}/Dockerfile" -m "version ${tag}"
+    git tag -f "${tag}"
+done
+
+
