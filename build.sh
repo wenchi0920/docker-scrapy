45a9e6273756692b53b5459e5cf4a6d3c3172dd6
wenchi <wenchi@sitetag.us>
2020-10-13 11:50:24 +0800

    build tag shell

--- build.sh
+++ build.sh
@@ -8,13 +8,16 @@ IFS=$'\n'
 for tag in `cat "${dname}/tag" | sort` ;
 do
     echo "${tag}"
-    cat "${dname}/Dockerfile.sample" | sed "s/{{VER}}/$tag/" > "${dname}/Dockerfile"
-    git commit "${dname}/Dockerfile" -m "version ${tag}"
-    git tag -d "${tag}"
-    git push origin ":${tag}"
-    git tag "${tag}"
-    git push origin master
-    git push --tag
+    if [ `git tag 2>&1 | grep "${tag}" | wc -l` -eq "0" ] ;
+    then
+        cat "${dname}/Dockerfile.sample" | sed "s/{{VER}}/$tag/" > "${dname}/Dockerfile"
+        git commit "${dname}/Dockerfile" -m "version ${tag}"
+        git tag -d "${tag}"
+        git push origin ":${tag}"
+        git tag "${tag}"
+        git push origin master
+        git push --tag
+    fi
 done
 
 
